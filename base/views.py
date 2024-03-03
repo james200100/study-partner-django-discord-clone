7d6185aca9314465f5443618361fb6407ef749bc
james200100 <raj.20b1531084@abes.ac.in>
2024-03-03 23:16:57 +0530

    first urls and view routing

---
+++ base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def room(request):
+    return HttpResponse('Room')
+
+# Create your views here.
