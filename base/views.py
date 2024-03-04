1ddcd3d9df35d455c56c79b98649561ddadb054e
james200100 <raj.20b1531084@abes.ac.in>
2024-03-04 20:26:58 +0530

    reder the data and create database models

--- base/views.py
+++ base/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms=[
+"""rooms=[
     {'id':1,'name':'Lets learn python'},
     {'id':2,'name':'Lets learn Django'},
     {'id':3,'name':'Frontend Developer'},
     {'id':4,'name':'Backedn Developer'}
 
-]
+]"""
 
 def home(request):
+    rooms=Room.objects.all()
     context={'rooms':rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'base/room.html',context)
 
